c5909bf0292a0b1095698d7e32143a7fb36acc18
shivam yadav <shivamyadav0631@gmail.com>
2025-08-29 11:15:56 +0530

    update

--- DA LAb/quest.java
+++ DA LAb/quest.java
@@ -1,12 +1,6 @@
 import java.util.*;
 public class quest {
      
-    static void display(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the size of array");
@@ -15,17 +9,16 @@ public class quest {
         int[] arr= new int[n];
         for(int i=0;i<arr.length;i++){
             arr[i]= sc.nextInt();
+            System.out.print(arr[i]+" ");
         }
-
-        int mid= n/2;
         
           int sum1=0;
-         for(int i=0;i<mid;i++){
+         for(int i=0;i<n/2;i++){
             sum1= sum1+arr[i];
         }
         System.out.println("sum1 "+sum1);
          int sum2=0;
-         for(int i=mid;i<n;i++){
+         for(int i=n/2;i<n;i++){
             sum2= sum2+arr[i];
         }
          System.out.println("sum2 "+sum2);
